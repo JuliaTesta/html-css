892a13e45f7c7167861ca13528d7c464448203fb
Julia Testa Matos <jutemts@gmail.com>
2025-04-15 11:28:05 -0300

    Initial commit

---
+++ exercicios/ex002/comandos de entrada.cs
@@ -0,0 +1,32 @@
+Comandos de entrada
+  -  Console.ReadLine(); -> Leia a linha
+
+comandos de saída
+  - Console.WriteLine(); -> Escreve uma linha
+
+VARIÁVEIS: armazenamento de dados no computador
+
+Processamento: 
+
+Tipos de Variaveis 
+ - Numérica:
+      - Inteiro: idade
+      - Real/Decimal: salário 
+
+  - Literais:
+     - Letra: a, A, b, B
+     - Texto: algoritmo, código
+
+variaveis -> Memória RAM (temporária)
+
+
+
+//variavel para armazenar valor da idade
+Console.WriteLine("Hello World")
+var names = new[] { "Ana", "Felipe", "Emillia" };
+foreach (var name in names)
+{
+   Console.WriteLine($"Hello {name}");
+}
+
+
